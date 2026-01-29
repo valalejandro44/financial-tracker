3659bd9c506edeb70afd3fb6bc0571a76aaebb27
Luis Alejandro <luisalejandrordc@gmail.com>
2026-01-29 18:53:58 -0500

    Add postgresql database URL

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,19 @@
+#DATABASE_URL = "postgresql://luisalejandrordc:Barca2004#@localhost:5432/todo_db"
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+#from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://luisalejandrordc:Barca2004#@localhost:5432/todo_db"
+#load_dotenv()  # loads .env locally (Render ignores this)
+
+DATABASE_URL = "postgresql://testing_user:zsgltFVdDzdp8V8kPCtVPjQHXOmp26rU@dpg-d5tuumi4d50c73bdmn7g-a.oregon-postgres.render.com/todo_app_2yz0"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
---
+++ backend/test.py
@@ -0,0 +1,13 @@
+import psycopg
+from psycopg.errors import OperationalError
+
+DATABASE_URL = "postgresql://testing_user:zsgltFVdDzdp8V8kPCtVPjQHXOmp26rU@dpg-d5tuumi4d50c73bdmn7g-a.oregon-postgres.render.com/todo_app_2yz0"
+
+try:
+    with psycopg.connect(DATABASE_URL, connect_timeout=10) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT 1")
+            print("✅ Connected to Render PostgreSQL")
+except OperationalError as e:
+    print("❌ Connection failed")
+    print(e)
